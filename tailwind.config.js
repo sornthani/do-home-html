011662893fe994daab81709280d886cd3ce36abe
sornthani <thanisornnn@gmail.com>
2024-08-08 16:00:16 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        pink: "#fa0054",
+        orange: "#ff5a00",
+        green: "#00a400",
+        blue: "#0038f9",
+      },
+    },
+  },
+  plugins: [],
+};
